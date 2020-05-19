19d0ba73f687e5316d7bcb4cd0f107c62103bdff
Llumine <61595099+Llumine@users.noreply.github.com>
2020-05-19 16:49:50 +0200

    1.5.1
    Ajoute effet visuel du gong sur le script gongwav

--- Assets/Scripts/Enemies/GongWave.cs
+++ Assets/Scripts/Enemies/GongWave.cs
@@ -7,6 +7,8 @@ public class GongWave : MonoBehaviour
 {
     private Hunter _hunter;
     private Vivant targetVie;
+    public GameObject GauchePart;
+    public GameObject DroitePart;
 
     [SerializeField] private float damage = 5f;
     
@@ -18,6 +20,7 @@ public class GongWave : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("hikiki");    
         IClochePropag bro = other.gameObject.GetComponent<IClochePropag>();
         
         if (other.gameObject.CompareTag("Player"))
@@ -29,6 +32,16 @@ public class GongWave : MonoBehaviour
         {
             if (bro != null)
             {
+                
+                Destroy(
+                Instantiate(GauchePart, transform.position,
+                Quaternion.FromToRotation(Vector3.forward, transform.position)),
+                1);
+                Destroy(
+                Instantiate(DroitePart, transform.position,
+                Quaternion.FromToRotation(Vector3.forward, transform.position)),
+                1);
+        
                 Debug.Log("lets go !!!!");
                 bro.propagOnde();
                 FMODUnity.RuntimeManager.PlayOneShot("event:/Event3D/EnnemiDistance3D/Cloches/Ennemi_Cloche",transform.position);

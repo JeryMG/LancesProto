d4c2c369b07cc32b321e2e20113095d36d3ea16f
Mabo Jerome <jeromemabo@gmail.com>
2020-05-26 19:38:42 +0200

    Kubes tombant

--- Assets/Scripts/ParametresJeux/testObjets/platformD.cs
+++ Assets/Scripts/ParametresJeux/testObjets/platformD.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 public class platformD : MonoBehaviour
 {
     public float fadeTime;
-    
+    public Animator animator;
+    private MeshCollider meshCollider;
+
+    private void Start()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -17,7 +24,15 @@ public class platformD : MonoBehaviour
 
     IEnumerator disappearX(float seconds)
     {
+        Vector3 pos = transform.position;
         yield return new WaitForSeconds(seconds);
-        gameObject.SetActive(false);
+        animator.SetBool("Drop", true);
+        yield return new WaitForSeconds(1.3f);
+        transform.parent.gameObject.SetActive(false);
+        animator.SetBool("Drop", false);
+        transform.position = pos;
+        meshCollider.convex = false;
+        meshCollider.isTrigger = false;
+
     }
 }
